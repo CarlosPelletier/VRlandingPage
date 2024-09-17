91582cba0a7be0e8667d65ac78312861e74b915f
Carlos Esparza <carlos.esparzapelletier389@gmail.com>
2024-09-17 14:13:29 -0600

    Proyecto terminado

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */  //Configuración por dafault de la pagina de Tailwind
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
